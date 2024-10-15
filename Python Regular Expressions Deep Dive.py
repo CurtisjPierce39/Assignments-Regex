44ab9c4678a75d9c9dff243b6e708afc0bb84bd9
Curtis J Pierce <Curtisjpierce39@gmail.com>
2024-10-15 09:18:37 -0700

    add initial description

---
+++ Python Regular Expressions Deep Dive.py
@@ -0,0 +1,7 @@
+#Questions 1 Task 1 
+
+import re
+
+text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
+emails = re.findall(r"\b[A-Za-z0-9._%+-]+@[domain]+\.[A-Z|a-z]{2,}\b", text)
+print(emails)
